1afd12f8a510295eb97764aaa90418bc2a1f66b8
Andrew Davis <tuf75588@temple.edu>
2018-04-30 20:54:11 -0400

    progress

--- freeCodeCamp-challenges/advanced/telephoneCheck.js
+++ freeCodeCamp-challenges/advanced/telephoneCheck.js
@@ -1,18 +1,22 @@
 function telephoneCheck(str) {
   // Good luck!
   //check if first index is less than 1.
-  if (str[0] === '-') return false;
-  let formatted = str.replace(/-|\s/g, "");
+  if (str[0] === "-" || parseInt(str[0] > 1)) return false;
+  let formatted = str.replace(/[- )(]/g, "");
+  if (str[0] === "1") {
+    let check = [...formatted]
+      .map(num => parseInt(num))
+      .every(x => Number.isInteger(x));
+    if (check) return true;
+  }
   //if the number is less than 10 in length, don't even continue.
   if (formatted.length < 10) return false;
   //make deep copy of array to check parenthesis cases.
-  let formattedArray = [...formatted]
-  console.log(formattedArray.indexOf('('), formattedArray.indexOf(')'))
-  //if parens are at index 1 and 5, or 0 and 4 do some stuff.
-  if (formattedArray.indexOf('(') === 0 && formattedArray.indexOf(')') === 4 || formattedArray.indexOf('(') === 1 && formattedArray.indexOf(')') === 5) {
-
-  }
-
+  return [...formatted]
+    .map(num => parseInt(num))
+    .every(x => Number.isInteger(x))
+    ? true
+    : false;
 }
 
 console.log(telephoneCheck("1-(555)-555-5555")); //should return true
