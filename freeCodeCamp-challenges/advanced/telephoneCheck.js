7b6aac3d19cbe1772dffc7f969f3cfb23162dc5a
Andrew Davis <tuf75588@temple.edu>
2018-05-03 23:22:45 -0400

    non-regex

--- freeCodeCamp-challenges/advanced/telephoneCheck.js
+++ freeCodeCamp-challenges/advanced/telephoneCheck.js
@@ -1,9 +1,14 @@
 function telephoneCheck(str) {
   // Good luck!
+  //make sure first number is at least greater than 0.
   if (str[0] === "-") return false;
+  //if less than 10 characters, dont even continue
+  if (str.length < 10) return false;
   let formattedArray = [...str];
   //if parens are at index 0 and 4;
-
+  if (formattedArray.indexOf("(") === 0 && formattedArray.indexOf(")") === 4) {
+    console.log(formattedArray);
+  }
   return formattedArray;
 }
 
@@ -12,3 +17,4 @@ console.log(telephoneCheck("555-5555")); // should return false
 console.log(telephoneCheck("-1 (757) 622-7382")); //should return false
 console.log(telephoneCheck("(555)555-5555")); //should return true
 console.log(telephoneCheck("10 (757) 622-7382")); //should return true
+console.log(telephoneCheck("1 555)555-5555")); //should return true
