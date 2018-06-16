fa827a1ea8471f91496503d580bb0f76cd3f0733
Andrew Davis <tuf75588@temple.edu>
2018-06-15 21:53:12 -0400

    new test case

--- freeCodeCamp-challenges/advanced/cashRegister.js
+++ freeCodeCamp-challenges/advanced/cashRegister.js
@@ -1,20 +1,17 @@
 function checkCashRegister(price, cash, cid) {
-  //purchase price = price
-  //payment = cash
-  //cash in drawer = cid
-  console.log(cash - price);
   let totals = cid.map(total => total[1]).reduce((a, b) => a + b);
-  if (totals < cash - price) {
-    return {
-      status: "INSUFFICIENT_FUNDS",
-      change: []
-    };
+  const change = cash - price;
+  const exactChangeCheck = [...cid].slice(0,4).map(num => num[1]).reduce((a,b) => a + b);
 
-  }
+  //if change is the same as all the cash in the register
+  if (totals === change) return {status: "CLOSED", change: cid}
+  //insufficient funds cases
 
-  const change = [];
-  return change;
+  
+   
+  
 }
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 
 // Example cash-in-drawer array:
 // [["PENNY", 1.01],
@@ -27,16 +24,17 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-]);
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100]
+// ]);
 
-//checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
